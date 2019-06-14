d40632f78b8bb38d9cd96f979a6a69b51696a541
blastanks <blastanks2@gmail.com>
2019-06-14 11:35:52 +1000

    Added Function for Fly Zone and Warning if Waypoint is in Obstacle Range

--- generate_flight_path.py
+++ generate_flight_path.py
@@ -17,7 +17,8 @@ def extractMissionData(mission_data_input):
 
     # Importing the Data from the .json file
     with open(mission_data_input,'r') as data_f:
-    	mission_data = json.load(data_f)
+        print(mission_data_input)
+        mission_data = json.load(data_f)
 
     waypoints = []
 	# Extract the Latitude and Longitude from each Waypoint
@@ -31,7 +32,12 @@ def extractMissionData(mission_data_input):
         exclusionPoints.append(np.array([ obstacle["latitude"], obstacle["longitude"] ]))
         exclusionRadius.append(obstacle["radius"])
 
-    return (waypoints, exclusionPoints, exclusionRadius)
+    # Extract the Boundary Points that Define the Flyable Zone
+    boundaryPoints = []
+    for boundaryPoint in mission_data["flyZones"][0]["boundaryPoints"]:
+        boundaryPoints.append(np.array([ boundaryPoint["latitude"], boundaryPoint["longitude"] ]))
+
+    return (waypoints, exclusionPoints, exclusionRadius, boundaryPoints)
 
 ### NOTE ###
 # Coordinates of All Points should be given as:
@@ -112,7 +118,7 @@ def calculateBearing(firstPoint, secondPoint):
 
     return bearing
 
-# Obtains Second Point given Distance and Bearing from First Point
+# Obtains Second Point given Distance and Bearingdefined by the First and Third Points
 def pointFromDistBear(firstPoint, thirdPoint, distance):
     ### INPUTS ###
     # firstPoint - 2x1 numpy.array - coordinate Vector to First Point
@@ -143,9 +149,100 @@ def pointFromDistBear(firstPoint, thirdPoint, distance):
 
     return secondPoint
 
+# Obtains Second Point given Distance and Bearing from First Point
+def pointFromDistBear2(firstPoint, bearing, distance):
+    ### INPUTS ###
+    # firstPoint - 2x1 numpy.array - coordinate Vector to First Point
+    # bearing - float - Bearing Angle from First Point to Second
+    # distance - Float - Distance from First to Second Point
+    ### OUTPUT ###
+    # secondPoint - 2x1 numpy.array - Coordinate Vector to Second Point
+
+    # Calculations: https://www.sunearthtools.com/tools/distance.php
+
+    R = 6372.795477598 # Radius of Earth in km
+    distance = distance / 3028.84
+
+    lat1 = math.radians(firstPoint[0]);
+    lon1 = math.radians(firstPoint[1]);
+
+    lat2 = math.asin( math.sin(lat1)*math.cos(distance/R) +
+         math.cos(lat1)*math.sin(distance/R)*math.cos(bearing))
+
+    lon2 = lon1 + math.atan2(math.sin(bearing)*math.sin(distance/R)*math.cos(lat1),
+                 math.cos(distance/R)-math.sin(lat1)*math.sin(lat2))
+
+    lat2 = math.degrees(lat2)
+    lon2 = math.degrees(lon2)
+
+    secondPoint = [lat2, lon2]
+
+    return secondPoint
+
+def closestDistance(firstPoint, secondPoint, keyPoint):
+    # Splitting Lattitudes and Longitudes of Input Points into Individual Terms
+    x0 = keyPoint[0]
+    y0 = keyPoint[1]
+    x1 = firstPoint[0]
+    y1 = firstPoint[1]
+    x2 = secondPoint[0]
+    y2 = secondPoint[1]
+
+    R = 6372.795477598 * 3028.84
+
+    # 1. First, find bearings
+    bearingAC = calculateBearing(firstPoint, keyPoint)
+    lineBearing = calculateBearing(firstPoint, secondPoint)
+
+    # 2. Find Distance between A and C
+    distanceAC = distanceTwoPoints(firstPoint, keyPoint)
+
+    # 3. Find Perpendicular Distance and Factor Tolerance
+    distance = math.asin( math.sin(distanceAC / R) * math.sin(bearingAC - lineBearing)) * R
+
+    return distance
+
+# Find Point on Line (with Fly Zone Tolerance) Closest to Another Key Point
+def closestPointonLine(firstPoint, secondPoint, keyPoint, tolerance):
+    ### INPUTS ###
+    # firstPoint - 2x1 numpy.array - coordinate Vector to First Point
+    # secondPoint - 2x1 numpy.array - Coordinate Vector to Second Point
+    # keyPoint - 2x1 numpy.array - Coordinate Vector to Key Point being Compared
+    # tolerance - Float - Tolerance for Closest Point from Line
+    ### OUTPUT ###
+    # closestPoint - 2x1 numpy.array - Coordinate Vector to Closest Point on Line with Tolerance
+
+    # Solution: https://stackoverflow.com/questions/20231258/minimum-distance-between-a-point-and-a-line-in-latitude-longitude
+
+    # Splitting Lattitudes and Longitudes of Input Points into Individual Terms
+    x0 = keyPoint[0]
+    y0 = keyPoint[1]
+    x1 = firstPoint[0]
+    y1 = firstPoint[1]
+    x2 = secondPoint[0]
+    y2 = secondPoint[1]
+
+    # 1. Find Perpendicular Distance and Factor Tolerance
+    distance = closestDistance(firstPoint, secondPoint, keyPoint)
+
+    # 2.  Determine if Point is on Left or Right Side of Point, and shift Bearing P-AB accordingly
+    lineBearing = calculateBearing(firstPoint, secondPoint)
+    sign = (x2 - x1)*(y0 - y1) - (x0 - x1)*(y2 - y1)
+    if sign < 0:
+        # Point is on Left Side of Point
+        pointBearing = lineBearing + (math.pi / 2)
+    else:
+        # Point is on Right Side of Point
+        pointBearing = lineBearing - (math.pi / 2)
+
+    # 3. Locate the Closest Point with Adjusted Distance
+    closestPoint = pointFromDistBear2(firstPoint, pointBearing, distance-tolerance)
+
+    return closestPoint
+
 # ===== Main Function =====
 #Determine Flight Path with Given Waypoints and Target Points
-def calculateFlightPath(waypoints, exclusionPoints, stepDistance, exclusionRadius, exclusionRadiusTolerance):
+def calculateFlightPath(waypoints, exclusionPoints, boundaryPoints, stepDistance, exclusionRadius, exclusionRadiusTolerance, flyZoneTolerance):
 	### INPUTS ###
 	# waypoints - nx1 numpy.array - List of Waypoint Coordinate Vectors
 	# exclusionPoints - nx1 numpy.array - List of Centres for Exclusion Zones
@@ -186,8 +283,17 @@ def calculateFlightPath(waypoints, exclusionPoints, stepDistance, exclusionRadiu
 			# 2d. Iterate for the other target points
         flightPath.append(waypoints[i + 1])
 
+        # 2e. Check if Waypoints are within Exclusion Radius and Send Alert
+        switch = False
+        for k in range(len(exclusionPoints)):
+            waypointExclusionDistance = distanceTwoPoints(waypoints[i], exclusionPoints[k])
+            if waypointExclusionDistance < exclusionRadius[k]:
+                switch = True
+        if switch == True:
+            print("Waypoint " + str(i) + " is inside the boundaries of some obstacle/s!")
+
+
     # 3. If three consecutive waypoints are collinear, remove the middle waypoint.
-    removeIndices = []
     flightPathTemp = flightPath
     flightPath = [flightPathTemp[0]]
 
@@ -196,6 +302,13 @@ def calculateFlightPath(waypoints, exclusionPoints, stepDistance, exclusionRadiu
         bearing2 = calculateBearing(point, flightPathTemp[index+1])
         if bearing1 != bearing2 and not np.array_equal(point, flightPath[-1]):
             flightPath.append(point)
+
+    # 4. Check if waypoints are contained within the fly zone, and adjust as neccessary
+    for index, point in enumerate(flightPath):
+        for i in range(len(boundaryPoints) - 2):
+            flyZoneDistance = closestDistance(boundaryPoints[i], boundaryPoints[i + 1], point)
+            if flyZoneDistance < flyZoneTolerance:
+                point = closestPointonLine(boundaryPoints[i], boundaryPoints[i + 1], point, flyZoneTolerance)
 
     return flightPath
 
@@ -215,17 +328,17 @@ def exportMission(waypointType, flightPath, default_alt, header_name):
             "{:.6f}".format(point[0]) + "\t" + "{:.6f}".format(point[1]) + "\t" + "{:.6f}".format(default_alt) +
             "\t" + str(1) + "\n")
 
-def main(mission_data, stepDistance, exclusionRadiusTolerance, waypointType, default_alt, header_name):
+def main(mission_data, stepDistance, exclusionRadiusTolerance, flyZoneTolerance, waypointType, default_alt, header_name):
 	### INPUTS ###
 	# mission_data - dictionary - Received Mission Data
     ### OUTPUT ###
     # mpmission.waypoint - waypoint file - Waypoint File for Mission Planner
 
     # 1. Import Mission Data
-    waypoints, exclusionPoints, exclusionRadius = extractMissionData(mission_data)
+    waypoints, exclusionPoints, exclusionRadius, boundaryPoints = extractMissionData(mission_data)
 
     # 2. Calculate the Flight Path
-    flightPath = calculateFlightPath(waypoints, exclusionPoints, stepDistance, exclusionRadius, exclusionRadiusTolerance)
+    flightPath = calculateFlightPath(waypoints, exclusionPoints, boundaryPoints, stepDistance, exclusionRadius, exclusionRadiusTolerance, flyZoneTolerance)
 
     # 3. Export the Mission to a File
     exportMission(waypointType, flightPath, default_alt, header_name)
@@ -233,22 +346,25 @@ def main(mission_data, stepDistance, exclusionRadiusTolerance, waypointType, def
 # ArgParse Default Values
 stepDistanceMin = 50.0 # in feet
 exclusionRadiusToleranceMin = 200.0 # in feet
+flyZoneToleranceMin = 100.0 # in feet
 default_alt_min = 150.0 # in feet
 
 # Parser Arguments
 parser = argparse.ArgumentParser(description = "Constructs a Flight Path from Given Waypoints and Obstacles")
 parser.add_argument("json_file", help = 'Input Mission Data .JSON File')
 parser.add_argument("--step", action = "store", dest = "stepDistance", type = float, default = stepDistanceMin, help = "Step Distance between Target Point - Define in ft")
-parser.add_argument("--tolerance", action = "store", dest = "exclusionRadiusTolerance", type = float, default = exclusionRadiusToleranceMin, help = "Tolerance for Exclusion Radius around Obstacles - Define in ft")
+parser.add_argument("--exclusionTolerance", action = "store", dest = "exclusionRadiusTolerance", type = float, default = exclusionRadiusToleranceMin, help = "Tolerance for Exclusion Radius around Obstacles - Define in ft")
+parser.add_argument("--zoneTolerance", action = "store", dest = "flyZoneTolerance", type = float, default = flyZoneToleranceMin, help = "Tolerance around Fly Zone Perimeter - Define in ft")
 parser.add_argument("--altitude", action = "store", dest = "default_alt", type = float, default = default_alt_min, help = "Default Altitude for Completing Mission - Define in ft")
 
 results = parser.parse_args()
 mission_data = results.json_file
 stepDistance = results.stepDistance
 exclusionRadiusTolerance = results.exclusionRadiusTolerance
+flyZoneTolerance = results.flyZoneTolerance
 default_alt = results.default_alt
 
 waypointType = 16
 header_name = "QGC WPL 110"
 
-main(mission_data, stepDistance, exclusionRadiusTolerance, waypointType, default_alt, header_name)
+main(mission_data, stepDistance, exclusionRadiusTolerance, flyZoneTolerance, waypointType, default_alt, header_name)
